e6be3347715c533e0190a73f233d06599a1b5f81
Wagner85 <oliveira.wagner@outlook.com>
2025-07-21 15:39:53 -0300

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // Adiciona o caminho base relativo para assets
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
